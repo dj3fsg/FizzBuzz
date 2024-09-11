1d5bca555d4d89e2b9845824b8473f7cb616d951
Satoshi Ishii <121612716+dj3fsg@users.noreply.github.com>
2024-09-11 21:20:05 +0900

    課題再提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,17 +5,17 @@ public class FizzBuzz {
 		//1から100までの数値をループで処理する
 		for (int i = 1 ; i <= 100 ; i++) {
 			
-			//3で割り切れる数値の場合、「Fizz」と表示する
-			if(i % 3 == 0 && i % 5 != 0) {
-				System.out.println("Fizz");
-				
-			//5で割り切れる数値の場合、「Buzz」と表示する
-			}else if (i % 3 != 0 && i % 5 == 0) {
-				System.out.println("Buzz");
-			
 			//3と5の両方で割り切れる数値の場合、「FizzBuzz」と表示する
-			}else if (i % 3 == 0 && i % 5 == 0) {
+			if(i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
+			
+			//3で割り切れる数値の場合、「Fizz」と表示する
+			}else if (i % 3 == 0 ) {
+				System.out.println("Fizz");
+			
+			//5で割り切れる数値の場合、「Buzz」と表示する
+			}else if (i % 5 == 0) {
+				System.out.println("Buzz");
 			
 			//それ以外の数値は、その数値をそのまま表示する
 			}else {
